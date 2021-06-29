8e7b1895b98b71b32bcea9dd5992bcf9713d8414
Derek Vangstad <vang0519@yahoo.com>
2021-06-29 18:04:59 -0500

    POST commit

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get("/api/notes", (req, res) => {
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
+app.post("/api/notes", (req,res) => {
+    res.json(db)
+})
 
 //listen to inputs
 app.listen(PORT, () => {
